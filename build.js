bf9c6cb5aba01ae19a38b1640c271b67d9412239
andreafalzetti <afalzetti@gmail.com>
2019-03-10 09:06:19 +0000

    linting con eslint+prettier

--- build.js
+++ build.js
@@ -1,21 +1,21 @@
-const fs   = require('fs');
+const fs = require('fs');
 const path = require('path');
 
-let dataPath = path.join(__dirname, 'data');
-let files = fs.readdirSync(dataPath);
+const dataPath = path.join(__dirname, 'data');
+const files = fs.readdirSync(dataPath);
 
-let data = [];
+const data = [];
 
-files.forEach((f) => {
-	let filePath = path.join(__dirname, 'data', f);
-	let content = fs.readFileSync(filePath).toString();
-	let obj = JSON.parse(content);
-	data.push(obj);
+files.forEach(f => {
+    const filePath = path.join(__dirname, 'data', f);
+    const content = fs.readFileSync(filePath).toString();
+    const obj = JSON.parse(content);
+    data.push(obj);
 });
 
 data.sort((a, b) => {
-	return a['nome'].localeCompare(b['nome']);
+    return a['nome'].localeCompare(b['nome']);
 });
 
-let outFilePath = path.join(__dirname, 'comuni.json');
+const outFilePath = path.join(__dirname, 'comuni.json');
 fs.writeFileSync(outFilePath, JSON.stringify(data));
